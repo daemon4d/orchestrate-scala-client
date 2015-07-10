b3209078d9554ccb7122446493ab1f9eecd1ff7c
daemon4d <daemon4d@gmail.com>
2015-07-10 19:31:48 +0300

    api key removed

--- src/test/scala/ru/quadcom/databaselib/orchestrate/scaldi/AppModule.scala
+++ src/test/scala/ru/quadcom/databaselib/orchestrate/scaldi/AppModule.scala
@@ -13,7 +13,7 @@ import scala.concurrent.duration.Duration
  */
 class AppModule extends Module {
   binding identifiedBy 'orchestrateApiURL to "https://api.orchestrate.io/v0"
-  binding identifiedBy 'applicationKey to "0680d860-5de6-44d1-94ba-a0e43f631e40"
+  binding identifiedBy 'applicationKey to "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
   bind[ActorSystem] to ActorSystem("Test") destroyWith ((as: ActorSystem) => Await.result(as.terminate(), Duration.Inf)) identifiedBy 'orchestrateClientAS
 
